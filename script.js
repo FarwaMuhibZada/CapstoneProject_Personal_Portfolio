0a3df780efc210d93c87419ba4a36d2ba9e79a54
farwaMuhibzada <farwafarid2017@gmail.com>
2024-07-14 14:49:53 +0430

    added navbar for moile screen.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menu');
-
+const navlinkEls = document.querySelectorAll('.nav-link');
 humburger.addEventListener ("click",() => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
@@ -11,6 +11,12 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click',(
   navMenu.classList.remove('active');
 }));
   
+navlinkEls.forEach((navlinkEl)=>{
+  navlinkEl.addEventListener('click',() => {
+    document.querySelector('.borders')?.classList.remove('borders');
+    navlinkEl.classList.add('borders');
 
+  })
+})
 
 
